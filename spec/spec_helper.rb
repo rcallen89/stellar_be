69c8cc119e9ae81857fab16aec70e759bb45164a
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-08 15:30:08 -0600

    Add Rack::Test::Methods to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,15 +33,17 @@ end
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-    config.include Capybara::DSL
+  config.include Rack::Test::Methods
 
-    config.before(:all) do
-        DatabaseCleaner.clean
-    end
+  config.include Capybara::DSL
 
-    config.after(:each) do
-        DatabaseCleaner.clean
-    end
+  config.before(:all) do
+      DatabaseCleaner.clean
+  end
+
+  config.after(:each) do
+      DatabaseCleaner.clean
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
