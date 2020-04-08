9397ecc0af1a45c49fb1c2b63540457e46216111
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-08 15:32:08 -0600

    Add two namespaces and a route

--- app/controllers/stellar_be_app.rb
+++ app/controllers/stellar_be_app.rb
@@ -1,4 +1,14 @@
 # This is essentially the Routes File!
-class StellarBeAPP < Sinatra::Base
+require "sinatra"
+require "sinatra/namespace"
 
-end
+# class StellarBeAPP < Sinatra::Base
+  namespace '/api' do
+    namespace '/v1' do
+      get "/humans" do
+        require "pry"; binding.pry
+        erb 'humans/show'.to_sym
+      end
+    end
+  end
+# end
