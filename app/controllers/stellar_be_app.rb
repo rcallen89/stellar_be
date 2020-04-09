d0abd735f287fcc85f84d5800d00adba8cb7ca49
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-09 14:48:26 -0600

    Add our routes we will go to in our FE app to get this data

--- app/controllers/stellar_be_app.rb
+++ app/controllers/stellar_be_app.rb
@@ -1,4 +1,19 @@
 # This is essentially the Routes File!
 class StellarBeAPP < Sinatra::Base
 
+  before do
+    content_type :json
+  end
+
+  get '/search' do
+    service = SolarSystemService.new
+    response = service.get_response(params[:query])
+    json(response)
+  end
+
+  get '/people_in_space' do
+    service = SpacePeopleService.new
+    response = service.get_data(params[:query])
+    json(response)
+  end
 end
