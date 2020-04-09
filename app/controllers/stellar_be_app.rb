30478d58eeef370638643173d200ac173c626954
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-08 20:20:05 -0600

    Deleted unnecessary routes

--- app/controllers/stellar_be_app.rb
+++ app/controllers/stellar_be_app.rb
@@ -1,14 +1,4 @@
 # This is essentially the Routes File!
-require "sinatra"
-require "sinatra/namespace"
+class StellarBeAPP < Sinatra::Base
 
-# class StellarBeAPP < Sinatra::Base
-  namespace '/api' do
-    namespace '/v1' do
-      get "/humans" do
-        require "pry"; binding.pry
-        erb 'humans/show'.to_sym
-      end
-    end
-  end
-# end
+end
