a03abef0c42bd539b6d9069346e842235b1da8a7
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-15 10:42:22 -0600

    Change methods in the services to class methods

--- app/services/solar_system_service.rb
+++ app/services/solar_system_service.rb
@@ -1,5 +1,5 @@
 class SolarSystemService
-  def get_response(name)
+  def self.get_response(name)
     response = Faraday.get("https://api.le-systeme-solaire.net/rest/bodies/#{name}")
     JSON.parse(response.body, symbolize_names: true)
   end
