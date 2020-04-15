14d2b31dee524d9b13604d7c429352989a8f8c76
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-15 10:42:05 -0600

    Changed service method calls to class methods in the stellar be controller to delete a line of code and look cleaner

--- app/controllers/stellar_be_app.rb
+++ app/controllers/stellar_be_app.rb
@@ -6,26 +6,22 @@ class StellarBeAPP < Sinatra::Base
   end
 
   get '/search' do
-    service = SolarSystemService.new
-    response = service.get_response(params[:query])
+    response = SolarSystemService.get_response(params[:query])
     json(response)
   end
 
   get '/people-in-space' do
-    service = SpacePeopleService.new
-    response = service.get_data
+    response = SpacePeopleService.get_data
     json(response)
   end
 
   get '/astronomy' do
-    service = MoonSunService.new
-    response = service.get_response(params[:query])
+    response = MoonSunService.get_response(params[:query])
     json(response)
   end
 
   get '/iotd' do
-    service = ImageOfTheDayService.new
-    response = service.get_image
+    response = ImageOfTheDayService.get_image
     json(response)
   end
 end
