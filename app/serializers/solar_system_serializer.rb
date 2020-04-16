800dbf429b8770663cd16961099ffdb45142251a
Ryan Allen <rcallen89@gmail.com>
2020-04-16 12:02:13 -0500

    Modify JSON for only needed data

--- app/serializers/solar_system_serializer.rb
+++ app/serializers/solar_system_serializer.rb
@@ -1,6 +1,6 @@
 class SolarSystemSerializer
 
   def self.data(response)
-    {data: response}.to_json
+    {data: response.slice(:englishName, :isPlanet, :moons, :aroundPlanet, :discoveredBy, :discoveryDate, :gravity)}.to_json
   end
 end
