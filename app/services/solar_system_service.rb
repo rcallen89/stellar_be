70d9d02c24eba170e31ca862e689ea03b1325b08
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-15 18:21:20 -0600

    Add helper method in service to return nil if the search term in invalid

--- app/services/solar_system_service.rb
+++ app/services/solar_system_service.rb
@@ -1,13 +1,20 @@
 class SolarSystemService
   def self.get_response(name)
     name = translate(name.capitalize)
-
     response = Faraday.get("https://api.le-systeme-solaire.net/rest/bodies/#{name}")
-    JSON.parse(response.body, symbolize_names: true)
+    check_body(response)
   end
 
   def self.translate(name)
     id = SolarSystemApi.where(eng_name: name).take
     id.object_id
+  end
+
+  def self.check_body(response)
+    if response.body == "Not found (entity)"
+      nil
+    else
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
